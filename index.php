ac2b14306c391c55ce4cfc499d1f89bf77ee06d8
bacoords <bacoords@gmail.com>
2016-06-10 08:39:09 -0700

    fix normalize

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/node_modules/normalize.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/node_modules/normalize.css/normalize.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/typeplate.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
     <title><?php wp_title( ' | ', true, 'right' ); ?></title>
